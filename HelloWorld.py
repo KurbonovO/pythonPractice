e419858050b90ea71c6bd0e7dca2f5b6ce478c5b
Otabek Kurbonov <Otabek.Kurbonov@cns-inc.com>
2019-03-07 12:47:45 -0500

    Getting started with the python.

---
+++ HelloWorld.py
@@ -0,0 +1,18 @@
+print("Hello, World!")
+
+#This is a comment.
+
+"""This is a 
+multiline docstring."""
+
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+"""Variables do not need to be declared with any particular type and
+can even change type after they have been set. """
+x = 4 # x is of type int
+x = "Sally" # x is now of type str
+print(x)
